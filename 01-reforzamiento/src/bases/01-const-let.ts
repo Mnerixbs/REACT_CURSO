0c66bcdcbe219d3d279c176c7013c11c9986ee12
Mario Rico <xbsneri@gmail.com>
2026-03-27 22:08:36 -0600

    01-variables

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const firstName = 'Mario';
+const lasName = 'Rico';
+
+let diceNumber = 5;
+diceNumber = '2';
+
+console.log(firstName,lasName);
+
+const containsLetterH = lasName.includes('R');
+
+console.log({containsLetterH}, diceNumber);
