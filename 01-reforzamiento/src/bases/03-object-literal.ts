9b49ab6dc8ce8b66a3a10864c9886263837692f5
Mario Rico <xbsneri@gmail.com>
2026-03-27 22:50:35 -0600

    Interfaces

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,20 +1,36 @@
-const Ironman = {
+interface Person{
+    firstName: string;
+    lastName: string;
+    age: number;
+
+    addres : Address;
+};
+
+interface Address{
+        postalCode: string,
+        city: string
+};
+
+const Ironman : Person = {
     firstName : 'Tony',
     lastName : 'Stark',
     age: 45,
     addres:{
-        postalCode: 'Ab21',
+        postalCode: '12345',
         city: 'New York'
     }
 };
 
-const spiderman = structuredClone(Ironman);
+
+//const spiderman = structuredClone(Ironman);
 
 
 
-spiderman.lastName = 'Parker'
-spiderman.firstName ='Peter'
-spiderman.age = 22;
-spiderman.addres.city = "Broklin"
+//spiderman.lastName = 'Parker'
+//spiderman.firstName ='Peter'
+//spiderman.age = 22;
+//spiderman.addres.city = "Broklin"
 
-console.log(Ironman, spiderman);
+//console.log(Ironman, spiderman);
+
+console.log(Ironman)
