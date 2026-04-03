c87ce4a307d3935b118561274a8fd2cba4924f70
Mario Rico <xbsneri@gmail.com>
2026-04-03 16:51:10 -0600

    Primer componente

---
+++ first-steps/src/FirstSteps.tsx
@@ -0,0 +1,15 @@
+export function FirstSrepsApp() {
+    return(
+        <>
+            <h1>Hola m</h1>
+    <p>Esto es un parrafo !!</p>
+
+    <button>Click me</button>
+
+    <div>
+      <h2>Esto es un div</h2>
+    </div>
+
+    </>
+    );
+}
